1ff43f6e36b53b0384fafcb16a05a30bf3c9b8f2
takeuchi kazuto <kazuto.takeuchi@kabuku.co.jp>
2019-06-13 10:27:32 +0900

    fix test

--- test/conversion.spec.ts
+++ test/conversion.spec.ts
@@ -3,11 +3,12 @@ import hsv2rgb from '../src/hsv2rgb'
 import { normalizeHsvOrDefault } from '../src/normalize'
 import { deepStrictEqual } from 'assert'
 
-test('hsv2rgb(rgb2hsv([r, g, b])) = [r, g, b]', () => {
+it('hsv2rgb(rgb2hsv([r, g, b])) = [r, g, b]', () => {
+  const defaultHsv = [NaN, NaN, NaN] as const
   for (let r = 0; r < 256; r++) {
     for (let g = 0; g < 256; g++) {
       for (let b = 0; b < 256; b++) {
-        deepStrictEqual(hsv2rgb(normalizeHsvOrDefault(rgb2hsv([r, g, b]), [])), [r, g, b])
+        deepStrictEqual(hsv2rgb(normalizeHsvOrDefault(rgb2hsv([r, g, b]), defaultHsv)), [r, g, b])
       }
     }
   }
