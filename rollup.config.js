8925e63e3127ffc17486148c407e02f3156ca2b6
luncheon <takeuck+github@gmail.com>
2020-03-11 22:38:12 +0900

    chore: update dependencies and config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import filesize from 'rollup-plugin-filesize'
 
 const config = (format, extension, compress, external) => ({
   input: `es/reinvented-color-wheel.js`,
@@ -11,9 +12,10 @@ const config = (format, extension, compress, external) => ({
     name: 'ReinventedColorWheel',
   },
   plugins: [
+    filesize({ showBrotliSize: true }),
     resolve(),
     commonjs(),
-    ...compress ? [terser({ toplevel: format === 'es', output: { semicolons: false }, warnings: true })] : [],
+    ...compress ? [terser({ toplevel: format === 'es', output: { semicolons: false }, warnings: true, compress: { passes: 3 } })] : [],
   ],
 })
 
